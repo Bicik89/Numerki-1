d65cb705bb66491ce681082bfa126c42a2536a8c
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-03-09 23:20:52 +0100

    Separate files

---
+++ HelperFunctions.py
@@ -0,0 +1,34 @@
+import math as m
+
+
+# Metoda Hornera: 'x' argument funkcji
+# 'wielomian' to lista ze współczynnikami w kolejności od najwyższej potęgi do zerowej (uważać na współczynnik 0)
+def horner(wielomian, x):
+    wartosc = wielomian[0]
+
+    for i in range(1,
+                   len(wielomian)):  # x^3 -2x^2 + 3x + 4 = x(x^2 - 2x + 3) + 4 = x(x(x - 2) + 3) - 4 -> działania po kolei:[[ w[0] * x + w[1] ] * x + w[2] ] * x + w[3]
+        wartosc = wartosc * x + wielomian[
+            i]  # 1*3 -2 = 1, then 1*3 + 3 = 6, then 6*3 + 4 = 22                                      [[ 1    * 3 + (-2) ] * 3 + 3    ] * 3 + 4
+
+    return wartosc
+
+
+# Wybór funkcji: 'i' oznacza wybraną funkcję, 'x' wartość argumentu
+# można inne zrobić if u care
+def getValue(i, x):
+    wn = [0.1, -2.0, 1.0, 13.0]  # na sztywno wpisane, by użyć w hornerze
+    if i == 1:  # 0.1x^3 -2x^2 + x + 13
+        return horner(wn, x)
+    elif i == 2:  # 3sin(x)
+        return 3 * m.sin(x)
+    elif i == 3:  # 3^x - 2
+        return (3) ** x - 2
+    elif i == 4:  # złożenie nr 1: sin(0.1x^3 -2x^2 + x + 13)
+        return m.sin(horner(wn, x))
+    elif i == 5:  # złożenie nr 2: 2^(0.1x^3 -2x^2 + x + 13) - 13
+        return 2 ** (horner(wn, x)) - 13
+    elif i == 6:  # 1.3^(13 * cos(x)) - 1
+        return 1.3 ** (13 * m.cos(x)) - 1
+        # else:  # zrobiłem tak że gdy użytkownik zjebie to bierze wielomian spod 1
+        # return horner(wn, x);
--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-import math as m
 import sys as s
 import matplotlib.pyplot as plot
 import numpy as num
-
+from HelperFunctions import getValue
 
 # also important jest zakaz float ale
 # Python's built-in float type has double precision (it's a C double in CPython, a Java double in Jython)
-
-# Metoda Hornera: 'x' argument funkcji
-# 'wielomian' to lista ze współczynnikami w kolejności od najwyższej potęgi do zerowej (uważać na współczynnik 0)
-
-def horner(wielomian, x):
-    wartosc = wielomian[0]
-
-    for i in range(1,
-                   len(wielomian)):  # x^3 -2x^2 + 3x + 4 = x(x^2 - 2x + 3) + 4 = x(x(x - 2) + 3) - 4 -> działania po kolei:[[ w[0] * x + w[1] ] * x + w[2] ] * x + w[3]
-        wartosc = wartosc * x + wielomian[
-            i]  # 1*3 -2 = 1, then 1*3 + 3 = 6, then 6*3 + 4 = 22                                      [[ 1    * 3 + (-2) ] * 3 + 3    ] * 3 + 4
-
-    return wartosc
-
-
-# Wybór funkcji: 'i' oznacza wybraną funkcję, 'x' wartość argumentu
-# można inne zrobić if u care
-def getValue(i, x):
-    wn = [0.1, -2.0, 1.0, 13.0]  # na sztywno wpisane, by użyć w hornerze
-    if i == 1:  # 0.1x^3 -2x^2 + x + 13
-        return horner(wn, x)
-    elif i == 2:  # 3sin(x)
-        return 3 * m.sin(x)
-    elif i == 3:  # 3^x - 2
-        return (3) ** x - 2
-    elif i == 4:  # złożenie nr 1: sin(0.1x^3 -2x^2 + x + 13)
-        return m.sin(horner(wn, x))
-    elif i == 5:  # złożenie nr 2: 2^(0.1x^3 -2x^2 + x + 13) - 13
-        return 2 ** (horner(wn, x)) - 13
-    elif i == 6:  # 1.3^(13 * cos(x)) - 1
-        return 1.3 ** (13 * m.cos(x)) - 1
-        # else:  # zrobiłem tak że gdy użytkownik zjebie to bierze wielomian spod 1
-        # return horner(wn, x);
 
 
 # krotka z human-readable nazwami funkcji
@@ -50,11 +16,11 @@ functions = ('0.1x\u00b3 - 2x\u00b2 + x + 13', '3sin(x)', '3\u02e3 - 2', 'sin(0.
 def bisection(i, start, end, stoperan, iteracjopsilon):
     # i - wybrana funkcja
     # start i end - krańce przedziału
-    # stoperan - przyjmuje dwie wartości i w zależności od niej warunkiem stopu jest osiagnięta dokładność (epsilon... ) lub liczba iteracji
+    # stoperan - przyjmuje dwie wartości i w zależności od niej warunkiem stopu jest osiagnięta dokładność (epsilon... ) albo liczba iteracji
     # iteracjopsilon - przechowuje wartość epsilonu lub liczby iteracji
 
-    startValue = float(getValue(i, start))  # wpisuje do zmiennej by wielokrotnie(całe dwa razy) tego nie wyliczac
-    endValue = float(getValue(i, end))  # wpisuje do zmiennej by wielokrotnie(całe dwa razy) tego nie wyliczac
+    startValue = float(getValue(i, start))  # wpisuje do zmiennej by wielokrotnie tego nie wyliczac
+    endValue = float(getValue(i, end))  # wpisuje do zmiennej by wielokrotnie tego nie wyliczac
     if (startValue * endValue > 0):
         print("Choosen section is incorrect.")
         s.exit()
@@ -124,12 +90,12 @@ def falsi(i, start, end, stoperan, iteracjopsilon):
 # falsi(i,start,end,stoperan,iteracjopsilon)
 def menufunctions():
     print("Choose which function's zero you want to find:")
-    print("1.Polynomial")
-    print("2.Trigonometric Function")
-    print("3.Exponential Function")
-    print("4.Composition of polynomial and trigonometric functions")
-    print("5.Composition of polynomial and exponential functions")
-    print("6.Composition of trigonometric and exponential functions")
+    print("1.Polynomial: " + functions[0])
+    print("2.Trigonometric Function: " + functions[1])
+    print("3.Exponential Function: " + functions[2])
+    print("4.Composition of polynomial and trigonometric functions: " + functions[3])
+    print("5.Composition of polynomial and exponential functions: " + functions[4])
+    print("6.Composition of trigonometric and exponential functions: " + functions[5])
 
 
 # Rysowanie wykresu funkcji
